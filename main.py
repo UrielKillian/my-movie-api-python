e69dfd3d56dc48147c3a052738e42e40727ff411
Edinson Cabrera Ñique <urielkillian2607@hotmail.com>
2023-01-05 00:44:23 -0500

    Configurando nuevos endpoints

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+import requests
+
+# Initializing the API
 
 app = FastAPI()
+app.title = "My first application with fastAPI"
+app.version = "0.0.1"
 
 
-@app.get('/')
+# Creating endpoints
+@app.get('/', tags=['Home'])
 def message():
-    return "Hello world"
+    return HTMLResponse('<h1>Hello World</h1>')
+
+
+@app.get('/pokemons', tags=['Pokemons'])
+def getAllPokemons():
+    # Connecting with the poke api
+    resp = requests.get('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0', timeout=1)
+    if resp.status_code != 200:
+        return "Cannot connect with the poke server"
+    data = resp.json()
+    return data
+
+
+@app.get('/pokemons/{name}', tags=['Pokemons'])
+def getPokemonByName(name: str):
+    # __Init__
+    data = None
+    # Searching the Pokemon with the id
+    resp = requests.get('https://pokeapi.co/api/v2/pokemon/'+name)
+    if resp.status_code != 200:
+        return "Cannot get the information of the pokemon"
+    print(resp.url)
+    data = resp.json()
+    return data
--- readme.md
+++ readme.md
@@ -1,5 +1,15 @@
 # Initialize the project
 
+## Create your own environment
+
+``py -m venv venv``
+
+Activate the environment
+
+``
+venv/Scripts/activate
+``
+
 ## Dependencies
 
 Install those dependencies
@@ -8,10 +18,25 @@ Install those dependencies
 pip install fastapi uvicorn
 ``
 
-Activate the environment
+or use this 
 
 ``
-venv/Scripts/activate
+pip install -r requirements.txt``
+
+If the file don't exist. You can create this file automatically using this command.
+
 ``
+pip freeze > requirements.txt``
+
 
 To start the project: ``uvicorn main:app --reload``
+
+To change the port: ``uvicorn main:app --reload --port ${number_of_the_port}``
+
+To make this available to all the devices of this red add the flag: ``--host 0.0.0.0``
+
+
+# Swagger in OpenAPI
+
+Enter to your localhost and add /docs to the url.
+http://localhost:8000/docs
